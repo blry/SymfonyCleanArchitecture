6ef294dcad23a55b5a0b6b6c99edb394e1918c4d
blry <alexander.sterpu@gmail.com>
2023-07-21 00:20:20 +0300

    Remove unused value objects

--- api/src/Infrastructure/Controller/Stream/V1/Streamers/Me/PatchGeneral/PatchGeneralBody.php
+++ api/src/Infrastructure/Controller/Stream/V1/Streamers/Me/PatchGeneral/PatchGeneralBody.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Controller\Stream\V1\Streamers\Me\PatchGeneral;
 
-use App\Domain\Stream\ValueObject\CountryEnum;
-use App\Domain\Stream\ValueObject\LanguageEnum;
-use App\Domain\Stream\ValueObject\StreamerCategoryEnum;
-use App\Domain\Stream\ValueObject\StreamerTagEnum;
 use Symfony\Component\Validator\Constraints as Assert;
 use OpenApi\Attributes as OA;
 
